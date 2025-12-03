2a8df1cd0ad5c206c0e0a06d3a987e439bc525f9
Albert <qooal@yandex.ru>
2025-12-03 20:18:59 +0700

    Checking the response date. The ability to add a comment without a reply.

--- pykam-qa.php
+++ pykam-qa.php
@@ -44,7 +44,10 @@ function pykam_qa_init() {
 }
 
 /**
- * 
+ * Initializes and outputs PykamQA component
+ *
+ * @param int $post_id Attached post ID. Use 0 for the current post
+ * @param int $count Maximum number of values to return. 0 = unlimited
  */
 function pykam_qa(int $post_id = 0, int $count = 0): void
 {
--- template-parts/qa-template.php
+++ template-parts/qa-template.php
@@ -4,24 +4,40 @@
  */
 
 if (!defined('ABSPATH')) exit;
+
+use \PykamQA\MetaBox;
+
+$question_author = get_post_meta( get_the_id(), MetaBox::QUESTION_AUTHOR, true );
+$answer_author = get_post_meta( get_the_id(), MetaBox::ANSWER_AUTHOR, true );
+$answer_date = get_post_meta( get_the_id(), MetaBox::ANSWER_DATE, true );
+$answer_content = get_post_meta(get_the_id(), MetaBox::ANSWER, true);
+
 ?>
 
 <div id="<?php the_ID() ?>" class="pykam-qa">
     <div class="pykam-qa-question-section">
         <div class="pykam-qa-header pykam-qa-question-header">
-            <span class="pykam-qa-date"><?php echo sprintf('%s: %s', __('Question Date', 'pykam-qa'), get_the_date()); ?></span>
-            <span class="pykam-qa-username"><?php echo sprintf('%s: %s', __('Question Author', 'pykam-qa'), get_post_meta( get_the_id(), '_pykam_qa_question_author', true )) ?></span>
+            <span class="pykam-qa-date">
+                <?php echo sprintf('%s: %s', __('Question Date', 'pykam-qa'), get_the_date()); ?>
+            </span>
+            <span class="pykam-qa-username">
+                <?php echo sprintf('%s: %s', __('Question Author', 'pykam-qa'), $question_author) ?>
+            </span>
         </div>
         <div class="pykam-qa-question">
             <?php the_content() ?>
         </div>
     </div>
-    <div class="pykam-qa-answer-section">
-        <div class="pykam-qa-header pykam-qa-question-header">
-            <span class="pykam-qa-date"><?php echo sprintf('%s: %s, %s', __('Respond', 'pykam-qa'), get_post_meta( get_the_id(), '_pykam_qa_answer_author', true ), date('d.m.Y', get_post_meta( get_the_id(), '_pykam_qa_answer_date', true ))) ?></span>
+    <?php if (trim($answer_content) !== "" && ($answer_date <= time())): ?>
+        <div class="pykam-qa-answer-section">
+            <div class="pykam-qa-header pykam-qa-question-header">
+                <span class="pykam-qa-date">
+                    <?php echo sprintf('%s: %s, %s', __('Respond', 'pykam-qa'), $answer_author, date('d.m.Y', $answer_date)) ?>
+                </span>
+            </div>
+            <div class="pykam-qa-answer">
+                <?php echo wp_kses_post($answer_content) ?>
+            </div>
         </div>
-        <div class="pykam-qa-answer">
-            <?php echo wp_kses_post(get_post_meta(get_the_id(), '_pykam_qa_answer_content', true)) ?>
-        </div>
-    </div>
+    <?php endif; ?>
 </div>
